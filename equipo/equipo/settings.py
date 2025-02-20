1d3c7edf15ad4d0b69ac044074c645cea7d8be8d
Praveen M V <36099643+praveenmv93@users.noreply.github.com>
2025-02-20 17:56:37 +0530

    Update settings.py

--- equipo/equipo/settings.py
+++ equipo/equipo/settings.py
@@ -114,16 +114,18 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = "static/"
-
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = "/static/"  # Corrected: Added leading slash
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media'),  # Or whatever folder your static files are in.
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
